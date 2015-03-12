d1fb3ed3d3e37a59ebb52de4607d97f9302d9a06
Alberto Valverde Gonzalez <alberto@meteogrid.com>
2015-03-12 12:40:09 +0100

    can load modules

--- System/DirWatch.hs
+++ System/DirWatch.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 module System.DirWatch where
 
-import Control.Applicative (pure, (<$>), (<*>))
+import Control.Applicative (<$>), (<*>))
 import Data.Aeson (
     FromJSON (..)
   , ToJSON (..)
@@ -18,10 +19,17 @@ import Data.Aeson (
   )
 import Data.Text (Text)
 import Data.Typeable (Typeable)
+import qualified Data.List.Split as L
 import qualified Data.HashMap.Strict as HM
 import qualified Data.ByteString.Lazy as LBS
 import System.FilePath.GlobPattern (GlobPattern)
-import Language.Haskell.Interpreter
+import Language.Haskell.Interpreter (
+    runInterpreter
+  , interpret
+  , as
+  , setImportsQ
+  , loadModules
+  )
 import Prelude
 
 data Config p h
@@ -75,16 +83,16 @@ instance FromJSON SerializableWatcher where
 
 data Spec
   = EvalSpec   String
-  | ImportSpec String c
+  | ImportSpec String String Object
   deriving (Show)
 
 instance ToJSON Spec where
   toJSON (EvalSpec s)
     = object ["eval" .= s]
-  toJSON (ImportSpec s c)
+  toJSON (ImportSpec m s c)
     = Object (HM.union os c)
     where
-      os = case object ["import" .= s] of
+      os = case object ["import" .= concat [m, ":", s]] of
             Object os' -> os'
             _          -> error "should never happen"
 
@@ -93,9 +101,11 @@ instance FromJSON Spec where
     mEval <- v .:? "eval"
     case mEval of
       Just s  -> return $ EvalSpec s
-      Nothing -> ImportSpec <$>
-                 v .: "import"
-                 <*> (pure (HM.delete "import" v))
+      Nothing -> do
+        i <- v .: "import"
+        case L.splitOn ":" i of
+          [m,s] -> return (ImportSpec m s (HM.delete "import" v))
+          _     -> fail "\"import\" should be <module>:<symbol>"
   parseJSON _ = fail "Expected an object"
 
 loadConfig :: SerializableConfig -> IO RunableConfig
@@ -111,12 +121,16 @@ loadWatcher w = do
   hs <- mapM loadSpec (wHandlers w)
   return $ w {wPreProcessor=mP, wHandlers=hs}
 
-loadSpec :: Typeable a => Spec -> IO a
+loadSpec :: forall a. Typeable a => Spec -> IO a
 loadSpec spec = do
   result <- runInterpreter $ do
     setImportsQ [("Prelude", Nothing)]
     case spec of
       EvalSpec s -> interpret s as
+      ImportSpec m s c -> do
+        loadModules [m]
+        o <- interpret s (as :: Object -> a)
+        return (o c)
   case result of
     Left  e -> error $ show e
     Right v -> return v
